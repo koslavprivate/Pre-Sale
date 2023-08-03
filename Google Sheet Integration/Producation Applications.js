d9f9e27cd88071f830508086d17a6279c59728c8
koslavprivate <koslavprivate@gmail.com>
2023-08-03 16:17:41 +0300

    update js file

--- Google Sheet Integration/Producation Applications.js
+++ Google Sheet Integration/Producation Applications.js
@@ -1,5 +1,5 @@
 let dataGoogleSheets;
-const PERSONAL_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL29yZ2Q1OWRiZThkLmNybTQuZHluYW1pY3MuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTNhODkyMmUtNDBiMC00ODA0LThlYjAtOTcxZjE1ZjczODc3LyIsImlhdCI6MTY5MDk1NjEzNiwibmJmIjoxNjkwOTU2MTM2LCJleHAiOjE2OTA5NjAwMzYsImFpbyI6IkUyRmdZR0E1R3IxZHlTbFYzaWJ0NExlTVd4T2ZBUUE9IiwiYXBwaWQiOiIxM2RmOThkYy05ZGZjLTRiMmItODI1NS1kY2EwZTE3MTg3NmYiLCJhcHBpZGFjciI6IjEiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9lM2E4OTIyZS00MGIwLTQ4MDQtOGViMC05NzFmMTVmNzM4NzcvIiwib2lkIjoiNWIxZDIzOTEtOGQxYy00ZGVjLWEwNGUtMGY4OTY5MDE1MTMyIiwicmgiOiIwLkFhOEFMcEtvNDdCQUJFaU9zSmNmRmZjNGR3Y0FBQUFBQUFBQXdBQUFBQUFBQUFDdkFBQS4iLCJzdWIiOiI1YjFkMjM5MS04ZDFjLTRkZWMtYTA0ZS0wZjg5NjkwMTUxMzIiLCJ0aWQiOiJlM2E4OTIyZS00MGIwLTQ4MDQtOGViMC05NzFmMTVmNzM4NzciLCJ1dGkiOiJqQi11ak9qSkFFeVp5c296QXFnT0FBIiwidmVyIjoiMS4wIiwieG1zX2NhZSI6IjEifQ.qxAofTKgkiKRP_YrxORG9rmKdgm5WCZZMfDWRL4tj5fxXQ-JqFcfoPJVxn-7ocnNVhE0inoG5dBbgh_B6C-CRaE12PKredK0xsW1dsWMpKR8Hc0JP2Ru_uQHPpWi6eszS-pSrkesodUnfFq9qFif5JSjb6Kvh1PoLg7VfYIOnkQwbdjkL12fnBlEXAUTZrTqggj7iWwJsa1NMokJUUl1z62o0dyIdCKT1bZGuHLcPnJhMFoHxqG9tOphALZKbeWIOF6mG9ajYWPiyGrnpPpmy8Vjnt9bzVB2nQ25wntYg2H2FZj6VzXMl6vHha3wAKzJOvkcz2mF-QXfvqhqcnCPIA";
+const PERSONAL_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL29yZ2Q1OWRiZThkLmNybTQuZHluYW1pY3MuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTNhODkyMmUtNDBiMC00ODA0LThlYjAtOTcxZjE1ZjczODc3LyIsImlhdCI6MTY5MTA2NjkyOCwibmJmIjoxNjkxMDY2OTI4LCJleHAiOjE2OTEwNzA4MjgsImFpbyI6IkUyRmdZSGg5dGUyUm0zV3ppWm5NVnNhY0IwSENBQT09IiwiYXBwaWQiOiIxM2RmOThkYy05ZGZjLTRiMmItODI1NS1kY2EwZTE3MTg3NmYiLCJhcHBpZGFjciI6IjEiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9lM2E4OTIyZS00MGIwLTQ4MDQtOGViMC05NzFmMTVmNzM4NzcvIiwib2lkIjoiNWIxZDIzOTEtOGQxYy00ZGVjLWEwNGUtMGY4OTY5MDE1MTMyIiwicmgiOiIwLkFhOEFMcEtvNDdCQUJFaU9zSmNmRmZjNGR3Y0FBQUFBQUFBQXdBQUFBQUFBQUFDdkFBQS4iLCJzdWIiOiI1YjFkMjM5MS04ZDFjLTRkZWMtYTA0ZS0wZjg5NjkwMTUxMzIiLCJ0aWQiOiJlM2E4OTIyZS00MGIwLTQ4MDQtOGViMC05NzFmMTVmNzM4NzciLCJ1dGkiOiJDQ3JPMXpodm8waUZ5TF9odXBZQ0FBIiwidmVyIjoiMS4wIiwieG1zX2NhZSI6IjEifQ.Tj32TngQO-Yx5wF5eNSXUOolBviI0n3cwx6IGR7IPdKHWxeVgEYG-hVlJe1QiXvI5w77KCI2xBFDCPFhAev6BlEsE9DBbqLdqGS8RYa34c3LVfrNInBnKSihIyThaEDOTjsX6k5zTG2mY361HU5g9EidrI31nxEqAjh7s2ZTVwVH71GWK22MB8tP8qvS5LmxcpAnrPE5KhJzt3B3jD0WUjpCyS4CIETXY96p35iu9odatsFtf5WemlTqxkYz7pHqdcde7y0Rr28C3KM_72HJymq9yPnFDdS7ZBmZmcrT2zN1mUUVkhyzcLvn6MvXiv9IaM2S0duNvKae3MiV8Vu90g";
 const API_URL = "https://orgd59dbe8d.api.crm4.dynamics.com/api/data/v9.2/";
 
 
@@ -23,52 +23,15 @@ function readData() {
    return content;
 }
 
-//not work
-function createRecord() {;
-  const DATA_ENDPOINT = "accounts";
-
-  const headers = {
-    "Authorization": "Bearer " + PERSONAL_ACCESS_TOKEN,
-    "Content-Type": "application/json",
-    "OData-MaxVersion": "4.0",
-    "OData-Version": "4.0",
-    "Accept": "application/json",
-    "Prefer": "return=representation"
-  };
-
-  const body = {
-    "cr254_name": "121231233"
-  };
-
-  const response = UrlFetchApp.fetch(API_URL + DATA_ENDPOINT, {
-       method: "POST",
-       headers: headers,
-       body: body
-   });
-  const responseData = JSON.parse(response.getContentText());
-  
-  // Получаем созданный идентификатор записи
-  const accountID = responseData.accountid;
-  // Выводим информацию о созданной записи
-  console.log("Created account with ID:", accountID);
-  console.log("Name:", responseData.name);
-  console.log("Description:", responseData.description);
-  console.log("Field 1:", responseData.field1);
-  console.log("Field 2:", responseData.field2);
-  console.log("Field 3:", responseData.field3);
-}
-
-
-
 function updateData() {
   
-   const HEADERS = ['Name', 'Field 1', 'Field 2', 'Field 3', 'PowerAppsId']
+   const HEADERS = ['Name', 'Field 1', 'Field 2', 'Field 3', 'Id', '__PowerAppsId__']
    const spreadsheet = SpreadsheetApp.getActive();
    const sheet = spreadsheet.getSheetByName('Аркуш1');
    sheet.clear();
    sheet.appendRow(HEADERS);
 
-  const headerRange = sheet.getRange(1, 1, 1, HEADERS.length);
+  const headerRange = sheet.getRange(1, 1, 1, HEADERS.length - 1);
   headerRange.setBackground('yellow');
 
    const content = readData();
@@ -88,7 +51,33 @@ function updateData() {
    }
 }
 
-function onEdit(e) {
+function cellsAreEmpty(row, col, sheet) {
+  if (!sheet.getRange(row, col).isBlank()) {
+    return false;
+  }
+
+  let resultColumns; 
+  if (col === 1) {
+    resultColumns = [2, 3, 4];
+  } else if (col === 2) {
+    resultColumns = [1, 3, 4];
+  } else if (col === 3) {
+    resultColumns = [1, 2, 4];
+  } else if (col === 4) {
+    resultColumns = [1, 3, 2];
+  } else {
+    return false;
+  }
+  
+  for(let i = 0; i < resultColumns.length; i++) {
+    if(!sheet.getRange(row, resultColumns[i]).isBlank()) {
+      return false;
+    }
+  } 
+  return true;
+}
+
+function onEditTrigger(e) {
   const spreadsheet = SpreadsheetApp.getActive();
   const sheet = spreadsheet.getSheetByName('Аркуш1');
   const oldValue = e.oldValue;
@@ -97,16 +86,114 @@ function onEdit(e) {
   const col = range.getColumn();
   const powerAppsIdValue = sheet.getRange(row, 5).getValue();
   
-  if (col === 5) {
-    PropertiesService.getScriptProperties().setProperty('recordsToDelete', oldValue);
-  } else {
-    range.setValue(PropertiesService.getScriptProperties().getProperty('recordsToDelete'))
+  if (cellsAreEmpty(row, col, sheet)) {
+    deleteRecord(powerAppsIdValue);
+    updateData();
+  }
+  else if (range.getNumRows() > 1 || range.getNumColumns() > 1) {
+    updateData();
+  }
+  else if (col === 5) {
+    deleteRecord(oldValue);
+    updateData();
+  }
+  else if(col < 5 && sheet.getRange(row, 5).isBlank()) {
+    createRecord(row, col, e.value);
+  } 
+  else if (col < 5) {
+    updateProperty(powerAppsIdValue, col, e.value);
+  }
+  else {
+    updateData();
   }
 }
 
-function processDeletions() {
-   deleteRecord(PropertiesService.getScriptProperties().getProperty('recordsToDelete'));
-   updateData();
+function createRecord(row, col, value) {
+    let property = PropertyName(col);
+    if (property === "cr254_field2") {
+      value = parseFloat(value);
+    }
+
+    const DATA_ENDPOINT = "cr254_producationapplications";
+    const headers = {
+      "Authorization": "Bearer " + PERSONAL_ACCESS_TOKEN,
+      "Content-Type": "application/json",
+      "OData-MaxVersion": "4.0",
+      "OData-Version": "4.0",
+      "Accept": "application/json",
+      "Prefer": "return=representation"
+    };
+
+    const body = {
+      [property] : value
+    };
+
+    const response = UrlFetchApp.fetch(API_URL + DATA_ENDPOINT, {
+        method: "POST",
+        headers: headers,
+        payload: JSON.stringify(body)
+    });
+    const responseData = JSON.parse(response.getContentText());
+    const idResult = responseData.cr254_producationapplicationid;
+    setCellValue(row, 5, idResult);
+  }
+
+  function updateProperty(id, col, val) {
+    let property = PropertyName(col);
+    if (property === "cr254_field2") {
+      val = parseFloat(val).toFixed(1);
+  }
+  
+  const DATA_ENDPOINT = `cr254_producationapplications(${id})/${property}`;
+  const headers = {
+    "Authorization": "Bearer " + PERSONAL_ACCESS_TOKEN,
+    "Content-Type": "application/json",
+    "OData-MaxVersion": "4.0",
+    "OData-Version": "4.0"
+  };
+
+  const body = {
+    value: val
+  }
+
+  const options = {
+    method: "PUT",
+    headers: headers,
+    payload: JSON.stringify(body)
+  };
+
+  const response = UrlFetchApp.fetch(API_URL + DATA_ENDPOINT, options);
+}
+
+function updateProperty(id, col, val) {
+  let property = PropertyName(col);
+  
+  if (val === undefined) {
+    val = null;
+  }
+  if (property === "cr254_field2") {
+    val = parseFloat(val);
+  }
+  
+  const DATA_ENDPOINT = `cr254_producationapplications(${id})/${property}`;
+  const headers = {
+    "Authorization": "Bearer " + PERSONAL_ACCESS_TOKEN,
+    "Content-Type": "application/json",
+    "OData-MaxVersion": "4.0",
+    "OData-Version": "4.0"
+  };
+
+  const body = {
+    value: val
+  }
+
+  const options = {
+    method: "PUT",
+    headers: headers,
+    payload: JSON.stringify(body)
+  };
+
+  const response = UrlFetchApp.fetch(API_URL + DATA_ENDPOINT, options);
 }
 
 function deleteRecord(id) {
@@ -124,6 +211,28 @@ function deleteRecord(id) {
    });
 }
 
+function PropertyName(col) {
+  if (col === 1) {
+    return "cr254_name";
+  }
+  else if (col === 2) {
+    return "cr254_field1";
+  }
+  else if (col === 3) {
+    return "cr254_field2";
+    val = parseFloat(val);
+  }
+  else if (col === 4) {
+    return "cr254_field3";
+  }
+}
+
+function setCellValue(row, col, value) {
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = spreadsheet.getSheetByName('Аркуш1'); // Замените на имя вашего листа
+  const cell = sheet.getRange(row, col);
+  cell.setValue(value);
+}
 
 
 
@@ -144,36 +253,6 @@ function deleteRecord(id) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// not work
 function getAccessToken() {
   eval(UrlFetchApp.fetch('https://alcdn.msauth.net/browser/2.14.2/js/msal-browser.min.js').getContentText());
   const msalConfig = {
---
+++ Plugins/PreSale/SendEmailByTemplate.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreSale
+{
+    public class SendEmailByTemplate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    Guid from = entity.Id;
+                    Guid to = new Guid("25a17064-1ae7-e611-80f4-e0071b661f01");
+                    Entity emailRecord = new Entity("email");
+                    emailRecord["from"] = from;
+                    emailRecord["to"] = to;
+
+                    SendEmailFromTemplateRequest emailUsingTemplateReq = new SendEmailFromTemplateRequest
+                    {
+                        Target = emailRecord,
+                        TemplateId = new Guid("8b249e77-0131-ee11-bdf4-0022489d36f2"),
+
+                        RegardingId = entity.Id,
+                        RegardingType = "contact"
+                    };
+                    SendEmailFromTemplateResponse emailUsingTemplateResp = (SendEmailFromTemplateResponse)service.Execute(emailUsingTemplateReq);
+                    Guid _emailId = emailUsingTemplateResp.Id;
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
